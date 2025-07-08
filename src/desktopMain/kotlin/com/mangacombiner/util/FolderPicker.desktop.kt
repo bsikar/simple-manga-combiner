e25abb04cc9ab47e2bcfe9f8a379d7e8eba02c49
Brighton Sikarskie <bsikar@tuta.io>
2025-07-07 23:07:49 -0500

    maded it use mac finder, will have to update later for different os

--- src/desktopMain/kotlin/com/mangacombiner/util/FolderPicker.desktop.kt
+++ src/desktopMain/kotlin/com/mangacombiner/util/FolderPicker.desktop.kt
@@ -1,16 +1,26 @@
 package com.mangacombiner.util
 
-import javax.swing.JFileChooser
+import java.awt.FileDialog
+import java.awt.Frame
 
 actual fun showFolderPicker(onFolderSelected: (path: String?) -> Unit) {
-    val chooser = JFileChooser().apply {
-        fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
-        dialogTitle = "Select Output Folder"
-    }
-    val result = chooser.showOpenDialog(null)
-    if (result == JFileChooser.APPROVE_OPTION) {
-        onFolderSelected(chooser.selectedFile.absolutePath)
-    } else {
-        onFolderSelected(null)
+    // This system property is specific to macOS to make the file dialog select directories.
+    System.setProperty("apple.awt.fileDialogForDirectories", "true")
+
+    try {
+        val dialog = FileDialog(null as Frame?, "Select Output Folder", FileDialog.LOAD)
+        dialog.isVisible = true
+
+        val directory = dialog.directory
+        val file = dialog.file
+
+        if (directory != null && file != null) {
+            onFolderSelected(directory + file)
+        } else {
+            onFolderSelected(null)
+        }
+    } finally {
+        // Reset the system property to avoid side effects for other Java apps.
+        System.setProperty("apple.awt.fileDialogForDirectories", "false")
     }
 }
